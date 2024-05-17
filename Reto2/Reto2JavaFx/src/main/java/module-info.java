8872b9c7825049832fb0e5c1edb1da0f0217beb9
010arcos <125090587+010arcos@users.noreply.github.com>
2024-05-17 14:02:29 +0200

    Delete Reto2 directory

--- Reto2/Reto2JavaFx/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.reto2javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.reto2javafx to javafx.fxml;
-    exports com.example.reto2javafx;
-}
