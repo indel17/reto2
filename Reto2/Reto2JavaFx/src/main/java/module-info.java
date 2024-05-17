0a1e76ae2be92ce464ddcc64e7bf8c17e6fa835d
010arcos <125090587+010arcos@users.noreply.github.com>
2024-05-17 19:43:40 +0200

    Add files via upload

---
+++ Reto2/Reto2JavaFx/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.reto2javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
+
+
+    opens com.example.reto2javafx to javafx.fxml;
+    exports com.example.reto2javafx;
+}
