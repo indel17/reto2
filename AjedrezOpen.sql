22695372c790aae60e47e326f27c297f38bf5b52
antoniorr00 <153596195+antoniorr00@users.noreply.github.com>
2024-05-23 13:11:18 +0200

    AjedrezOpen.sql

--- AjedrezOpen.sql
+++ AjedrezOpen.sql
@@ -11,7 +11,7 @@ create table jugador(
     fideID varchar(30),
     hotel boolean default false,
     CV boolean default false,
-    importeP int,/**/
+    importeP int, /*esta no haría falta pero podemos meter el importe del premio que se lleva el jugador*/
     categoria enum('A', 'B'),
     descalificado boolean default false,
     primary key(rankingI, categoria)
@@ -28,11 +28,11 @@ create table premio(
     categoria enum('A', 'B'),
     puesto int, 
     importe int,
-    primary key (tipo, categoria, puesto),
     rankingI int,
+    primary key (tipo, categoria, puesto),
+    constraint fk_premio_categoria foreign key (categoria) references torneo (categoria) on delete cascade on update cascade,
     constraint fk_premio_rankingI foreign key (rankingI) references jugador(rankingI) on delete cascade on update cascade
 );
-
 
 create table optarPremio(
     rankingI int,
@@ -40,29 +40,120 @@ create table optarPremio(
     tipo varchar(30),
     puesto int,
     constraint fk_optarPremio_jugador foreign key (rankingI, categoria) references jugador(rankingI, categoria) on delete cascade on update cascade,
-     constraint fk_optarPremio_premio foreign key (tipo, categoria, puesto) references premio(tipo, categoria, puesto) on delete cascade on update cascade,
+    constraint fk_optarPremio_premio foreign key (tipo, categoria, puesto) references premio(tipo, categoria, puesto) on delete cascade on update cascade,
     primary key (rankingI, categoria, tipo, puesto)
 );
-
 
 INSERT INTO torneo (categoria) VALUES ('A'), ('B');
 
 
-/*mirar la function o select para ver a que premios optan los jugadores*/
+insert into premio (categoria, tipo, puesto, importe) values 
+/* PREMIOS OPEN A */
+/* General */
+('A', "general", 1, 2300),
+('A', "general", 2, 1200),
+('A', "general", 3, 650),
+('A', "general", 4, 550),
+('A', "general", 5, 500),
+('A', "general", 6, 400),
+('A', "general", 7, 300),
+('A', "general", 8, 250),
+('A', "general", 9, 200),
+('A', "general", 10, 150),
+('A', "general", 11, 100),
+('A', "general", 12, 100),
 
+/* SUB2400 */
+('A', "sub2400", 1, 225),
+('A', "sub2400", 2, 150),
+('A', "sub2400", 3, 120),
+('A', "sub2400", 4, 100),
 
-/*Estos insert es para probar*/
-insert into  premio (categoria, tipo, puesto, importe) values 
-	('B', "general", 1, 1200),
-    ('B', "general", 2, 1000),
-    ('B', "sub1800", 1, 1100),
-    ('B', "sub1800", 2, 800),
-    ('B', "sub1600", 1, 900),
-    ('B', "sub1600", 2, 600),
-    ('B', "sub1400", 1, 150),
-    ('B', "sub1400", 2, 100), 
-    ('A', "general", 1, 1200),
-    ('A', "general", 2, 1000),
-    ('A', "sub2400", 1, 225),
-    ('A', "sub2200", 1, 150),
-	('A', "sub2200", 2, 100);
+/* SUB2200 */
+('A', "sub2200", 1, 150),
+('A', "sub2200", 2, 100),
+
+/* CV */
+('A', "cv", 1, 500),
+('A', "cv", 2, 400),
+('A', "cv", 3, 300),
+('A', "cv", 4, 200),
+('A', "cv", 5, 100),
+
+/* H */
+('A', "h", 1, 125),
+('A', "h", 2, 125),
+('A', "h", 3, 125),
+('A', "h", 4, 125),
+('A', "h", 5, 125),
+('A', "h", 6, 125),
+('A', "h", 7, 125),
+('A', "h", 8, 125),
+('A', "h", 9, 125),
+('A', "h", 10, 125),
+('A', "h", 11, 125),
+('A', "h", 12, 125),
+('A', "h", 13, 125),
+('A', "h", 14, 125),
+('A', "h", 15, 125),
+('A', "h", 16, 125),
+('A', "h", 17, 125),
+('A', "h", 18, 125),
+('A', "h", 19, 125),
+('A', "h", 20, 125),
+
+/* PREMIOS OPEN B */
+/* General */
+('B', "general", 1, 2300),
+('B', "general", 2, 1200),
+('B', "general", 3, 650),
+('B', "general", 4, 550),
+('B', "general", 5, 500),
+('B', "general", 6, 400),
+('B', "general", 7, 300),
+('B', "general", 8, 250),
+('B', "general", 9, 200),
+('B', "general", 10, 150),
+('B', "general", 11, 100),
+('B', "general", 12, 100),
+
+/* CV */
+('B', "cv", 1, 500),
+('B', "cv", 2, 400),
+('B', "cv", 3, 300),
+('B', "cv", 4, 200),
+('B', "cv", 5, 100),
+
+/* H */
+('B', "h", 1, 125),
+('B', "h", 2, 125),
+('B', "h", 3, 125),
+('B', "h", 4, 125),
+('B', "h", 5, 125),
+('B', "h", 6, 125),
+('B', "h", 7, 125),
+('B', "h", 8, 125),
+('B', "h", 9, 125),
+('B', "h", 10, 125),
+('B', "h", 11, 125),
+('B', "h", 12, 125),
+('B', "h", 13, 125),
+('B', "h", 14, 125),
+('B', "h", 15, 125),
+('B', "h", 16, 125),
+('B', "h", 17, 125),
+('B', "h", 18, 125),
+('B', "h", 19, 125),
+('B', "h", 20, 125),
+
+/* SUB1800 */
+('B', "sub1800", 1, 150),
+('B', "sub1800", 2, 100),
+
+/* SUB1600 */
+('B', "sub1600", 1, 150),
+('B', "sub1600", 2, 100),
+
+/* SUB1400 */
+('B', "sub1400", 1, 150),
+('B', "sub1400", 2, 100);
